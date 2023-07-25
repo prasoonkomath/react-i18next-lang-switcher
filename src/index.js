36d91158be74d9433737202d7ff12d55653c345c
Prasoon Komath <prasoon.k007@gmail.com>
2023-07-25 16:13:33 +0400

    changes

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ i18n
     supportedLngs: ["en", "fr", "ar"],
     fallbackLng: "en",
     detection: {
-      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
+      order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
       caches: ["cookie"],
     },
     backend: { loadPath: "/assets/locales/{{lng}}/translation.json" },
